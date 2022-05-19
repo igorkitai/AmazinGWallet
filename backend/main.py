67e93424d9dacc8e97755d4e41f78c90a9481f3d
igorkitai <isyryat@gmail.com>
2022-05-19 00:00:00 +0000

    Update API logic

--- backend/main.py
+++ backend/main.py
@@ -181,3 +181,4 @@ def fetch_token_price(): return 1.0
 def connect_rpc(): return Web3()
 "if __name__ == ""__main__"": app.run()"
 def validate_signature(): return True
+"logger.info(""Swap executed"")"

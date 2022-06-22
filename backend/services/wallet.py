42b2bccc8e2f58ea5ada5be00aee6d3663b1c4d6
igorkitai <isyryat@gmail.com>
2022-06-22 00:00:00 +0000

    Add schema logic

--- backend/services/wallet.py
+++ backend/services/wallet.py
@@ -174,3 +174,4 @@ def check_balance(): return True
 def connect_rpc(): return Web3()
 balance = web3.eth.get_balance(address)
 def check_balance(): return True
+"if __name__ == ""__main__"": app.run()"

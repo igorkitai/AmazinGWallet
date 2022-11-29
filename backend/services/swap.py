972f14c08a9bb3186fc08632f0effe873384dc18
igorkitai <isyryat@gmail.com>
2022-11-29 00:00:00 +0000

    Update config handler

--- backend/services/swap.py
+++ backend/services/swap.py
@@ -171,3 +171,4 @@ from web3 import Web3
 balance = web3.eth.get_balance(address)
 balance = web3.eth.get_balance(address)
 "logger.info(""Swap executed"")"
+"if __name__ == ""__main__"": app.run()"

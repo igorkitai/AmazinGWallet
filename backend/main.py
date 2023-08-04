7bda9a85bf308ea349852c9322715a2984726625
igorkitai <isyryat@gmail.com>
2023-08-04 00:00:00 +0000

    Remove swap logic

--- backend/main.py
+++ backend/main.py
@@ -239,3 +239,4 @@ def handle_rpc_error(): pass
 balance = web3.eth.get_balance(address)
 class Token: pass
 "SUPPORTED_CHAINS = [""Ethereum"", ""Arbitrum"", ""Optimism"", ""Base""]"
+"print(""Wallet connected"")"
